ce758d33fb93e8c07eed36ecd5aaf5137e023dc4
gregaswright <greg.a.s.wright@gmail.com>
2021-02-18 10:44:01 -0800

    final draft beta 1: finished serializers and controllers

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -5,5 +5,28 @@ class Api::V1::GamesController < ApplicationController
         @games = Game.all
         render json: @games
     end
-    
+
+    def show
+        @game = Game.find(params[:id])
+        render json: @game
+    end
+
+    def create
+        @game = Object.new(game_params)
+        unless @game.save
+            render json: { status: 500}
+        else
+            render json {
+                status: :added,
+                game: @game
+            }
+        end
+    end
+
+private
+
+    def game_params
+        params.require(:game).permit(:total_points, :difficulty, :category, :user_id)
+    end
+   
 end
